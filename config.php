1930f0567f30ebbaa6e6c915ede355b93020d84a
Christopher <75016606+cherysander07@users.noreply.github.com>
2024-06-24 06:32:45 -1100

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 $client_id = 'sO6APlSY3kkCwxYnGAVymbcIYbSDOEG6';
 $client_secret = 'KnJP10QsC2yJFkmekDSiy3LY7Vgnu9SYraMw1aYbhTMpO5dWRYQIIJnwTnqSNZZD';
 
-$mode = false; // add true if Production
+$mode = false; // add true if it's in production and change the client id and secret.
 
 ?>
